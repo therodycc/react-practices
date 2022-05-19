77cf533ac4f3037c95e9d48e69cab33858c5c33a
therodycc <therodycc@gmail.com>
2022-05-19 17:54:07 -0400

    .

--- src/pages/todo/index.tsx
+++ src/pages/todo/index.tsx
@@ -3,30 +3,10 @@ import Button from "../../components/common/button";
 import ListGroup from "../../components/common/ListGroup";
 import Layout from "../../components/layout";
 import { ListItemProps } from "../../interfaces/listItem/ListItem.interface";
+import { todoData } from "../../settings/todo/todo-data";
 
 const Todo = () => {
-    const [data, setData] = useState<Array<ListItemProps>>([
-        {
-            id: "02e7f1a0-444b-492d-851c-1fef05f53d2c",
-            title: "Do the homework",
-            active: false,
-        },
-        {
-            id: "32cd5a0d-a0b9-4185-b931-138ba8c95af0",
-            title: "Do the lunch",
-            active: false,
-        },
-        {
-            id: "fe0aab78-8687-4264-bb60-a89aeb423deb",
-            title: "Pay the car",
-            active: false,
-        },
-        {
-            id: "467077e0-c7ba-43ce-af90-40a7c6d79b57",
-            title: "Pay the house",
-            active: false,
-        },
-    ]);
+    const [data, setData] = useState<Array<ListItemProps>>(todoData);
 
     const [selectedItems, setSelectedItems] = useState<Array<ListItemProps>>([]);
 
---
+++ src/settings/todo/todo-data.ts
@@ -0,0 +1,22 @@
+export const todoData = [
+    {
+        id: "02e7f1a0-444b-492d-851c-1fef05f53d2c",
+        title: "Do the homework",
+        active: false,
+    },
+    {
+        id: "32cd5a0d-a0b9-4185-b931-138ba8c95af0",
+        title: "Do the lunch",
+        active: false,
+    },
+    {
+        id: "fe0aab78-8687-4264-bb60-a89aeb423deb",
+        title: "Pay the car",
+        active: false,
+    },
+    {
+        id: "467077e0-c7ba-43ce-af90-40a7c6d79b57",
+        title: "Pay the house",
+        active: false,
+    },
+]
